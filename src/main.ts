c2f0f798993b340d790daee6a1557a4607f85930
Daniel Lim <hbt@markhamboards.com>
2016-09-09 21:13:15 -0400

    intergrate angularfire2
    intergrate angularfire2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  // Initialize Firebase app  
+  defaultFirebase({
+    apiKey: "AIzaSyAt8czLnmYWB3wJi79u5aXM0T-LT2Vy5TA",
+    authDomain: "prolancers-b8a3e.firebaseapp.com",
+    databaseURL: "https://prolancers-b8a3e.firebaseio.com",
+    storageBucket: "prolancers-b8a3e.appspot.com",
+  })
+]);
