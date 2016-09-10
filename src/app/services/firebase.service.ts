9940281b39d7df0e84b4e1427884d28261a8745b
Daniel Lim <hbt@markhamboards.com>
2016-09-10 12:07:25 -0400

    Update Add Business Function
    Update Add Business Function

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -12,8 +12,21 @@ export class FirebaseService {
         
     }
     
-    getBusinesses() {
-        this.businesses = this.af.database.list('/businesses') as FirebaseListObservable<Business[]>
+    getBusinesses(category:string = null) {
+
+
+        if(category != null){
+          this.businesses = this.af.database.list('/businesses', {
+              query:{
+                  orderByChild: 'category',
+                  equalTo: category
+              }
+
+          }) as FirebaseListObservable<Business[]>
+        } else {
+            
+          this.businesses = this.af.database.list('/businesses') as FirebaseListObservable<Business[]>
+        }
         return this.businesses;
     }
     
