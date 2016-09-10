63004a34b4497df9cc7f4d762aae18a009c118f3
Daniel Lim <hbt@markhamboards.com>
2016-09-10 12:22:36 -0400

    add more to form
    add more to form

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrap(AppComponent, [
+  disableDeprecatedForms(),
+  provideForms(),  
   FIREBASE_PROVIDERS,
-  // Initialize Firebase app  
+  // Initialize Firebase App  
   defaultFirebase({
     apiKey: "AIzaSyAt8czLnmYWB3wJi79u5aXM0T-LT2Vy5TA",
     authDomain: "prolancers-b8a3e.firebaseapp.com",
